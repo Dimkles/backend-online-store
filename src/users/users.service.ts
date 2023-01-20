e9961df1a4d86874ba89e45544b12ce55329d296
Dimkles <dimkless.sharabaiko@gmail.com>
2023-01-20 21:17:03 +0300

    убрал консоль лог

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -15,7 +15,6 @@ export class UsersService {
     async createUser(dto: CreateUserDto) {
         const user = await this.userRepository.create(dto)
         const role = await this.rolesService.getRoleByValue('ADMIN')
-        console.log(role)
         await user.$set('roles', [role.id])
         user.roles = [role]
         return user
